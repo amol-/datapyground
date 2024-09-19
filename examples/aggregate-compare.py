f591815fbfe6a767b0bad7c7e336f20f7692b7c1
Alessandro Molina <alessandro@molina.fyi>
2024-09-19 19:05:49 +0000

    Add comparison to pandas

---
+++ examples/aggregate-compare.py
@@ -0,0 +1,58 @@
+import sys
+import time
+
+import pandas
+import psutil
+
+from datapyground.compute import AggregateNode, CSVDataSource, SumAggregation
+
+try:
+    aggregation_type = sys.argv[1]
+except IndexError:
+    aggregation_type = None
+
+if aggregation_type == "single":
+    # On development machine did lead to
+    #   TIME: 1.1 MEMORY: 167
+    q = AggregateNode(
+        ["year"],
+        {"total_geo_count": SumAggregation("geo_count")},
+        child=CSVDataSource("data/geounits.csv", block_size=1024 * 1024),
+    )
+elif aggregation_type == "multi":
+    # On aggregation_type machine did lead to
+    #   TIME: ?? MEMORY: ??
+    q = AggregateNode(
+        ["year", "Area"],
+        {"total_geo_count": SumAggregation("geo_count")},
+        child=CSVDataSource("data/geounits.csv", block_size=1024 * 1024),
+    )
+elif aggregation_type == "pandas":
+    # On development machine did lead to
+    #   TIME: 2.1 MEMORY: 116
+    class FakeQuery:
+        def batches(self):
+            df = pandas.read_csv("data/geounits.csv")
+            yield (
+                df.groupby("year")
+                .agg({"geo_count": "sum"})
+                .rename(columns={"geo_count": "total_geo_count"})
+            )
+
+    q = FakeQuery()
+else:
+    print("Aggregation must be single, multi or pandas")
+    sys.exit(1)
+
+proc = psutil.Process()
+start = time.time()
+for b in q.batches():
+    continue
+end = time.time()
+
+print(
+    "TIME:",
+    round(end - start, 1),
+    "MEMORY:",
+    proc.memory_full_info().rss // (1024 * 1024),
+)
--- examples/sorting-compare.py
+++ examples/sorting-compare.py
@@ -1,11 +1,16 @@
 import sys
 import time
 
+import pandas
 import psutil
 
 from datapyground.compute import CSVDataSource, ExternalSortNode, SortNode
 
-sorting_type = sys.argv[1]
+try:
+    sorting_type = sys.argv[1]
+except IndexError:
+    sorting_type = None
+
 if sorting_type == "external":
     # On development machine did lead to
     #   TIME: 84.4 MEMORY: 92
@@ -21,8 +26,17 @@ elif sorting_type == "memory":
     q = SortNode(
         ["year"], [True], CSVDataSource("data/geounits.csv", block_size=1024 * 1024)
     )
+elif sorting_type == "pandas":
+    # On development machine did lead to
+    #   TIME: 2.9 MEMORY: 409
+    class FakeQuery:
+        def batches(self):
+            df = pandas.read_csv("data/geounits.csv")
+            yield df.sort_values("year")
+
+    q = FakeQuery()
 else:
-    print("Sorting must be external or memory")
+    print("Sorting must be external, memory or pandas")
     sys.exit(1)
 
 proc = psutil.Process()
